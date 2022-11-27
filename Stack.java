54c526ca89a2c3c3911bdca32c4ce0b351def6b6
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-27 23:31:37 +0530

    creating stacks

---
+++ Stack.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.stackandqueues.stacks;
+
+
+public class Stack<T> {
+
+    LinkedList<T> linkedList;
+
+    public Stack() {
+        this.linkedList = new LinkedList<>();
+    }
+
+
+    public void push(T key) {
+        linkedList.push(key);
+    }
+
+
+    public void print(){
+        linkedList.display();
+    }
+
+    public T pop() {
+        return linkedList.pop();
+    }
+
+    public T peek(){
+        return linkedList.peek();
+    }
+
+    public boolean isEmpty(){
+        if(linkedList.size() == 0)
+            return true;
+        else
+            return false;
+    }
+}
