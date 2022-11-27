ec794ba6ba33faf4076536bbbd50f09ccf457fd9
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-27 23:32:43 +0530

    Main class

---
+++ StacksAndQueueMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.stackandqueues.stacks;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queues<Integer> queue = new Queues<>();
+        queue.enqueu(56);
+        queue.enqueu(30);
+        queue.enqueu(70);
+        queue.print();
+    }
+}
