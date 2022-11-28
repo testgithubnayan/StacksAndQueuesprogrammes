3cae008452e735697965c03b6ccff0ff34a1cf27
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-28 10:12:07 +0530

    Push Stacks

--- Stack.java
+++ Stack.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.stackandqueues.stacks;
 
+import com.bridgelabz.stackandqueues.linkedlist.*;
 
-public class Stack<T> {
+  public class Stack<T> {
 
     LinkedList<T> linkedList;
 
@@ -21,16 +22,5 @@ public class Stack<T> {
 
     public T pop() {
         return linkedList.pop();
-    }
-
-    public T peek(){
-        return linkedList.peek();
-    }
-
-    public boolean isEmpty(){
-        if(linkedList.size() == 0)
-            return true;
-        else
-            return false;
     }
 }
