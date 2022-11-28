4c81c2485b072875f2c376b9fb6a9a2daae12a4e
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-28 10:14:48 +0530

    Update StacksAndQueueMain.java

--- StacksAndQueueMain.java
+++ StacksAndQueueMain.java
@@ -1,12 +1,19 @@
 package com.bridgelabz.stackandqueues.stacks;
 
-public class Main {
+public class StacksAndQueueMain {
 
     public static void main(String[] args) {
-        Queues<Integer> queue = new Queues<>();
-        queue.enqueu(56);
-        queue.enqueu(30);
-        queue.enqueu(70);
-        queue.print();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.print();
+        System.out.println();
+        while(!stack.isEmpty()){
+            System.out.println(stack.peek());
+            stack.pop();
+            stack.print();
+            System.out.println();
+        }
     }
 }
