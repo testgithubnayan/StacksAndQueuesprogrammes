1a1772f2a1d74928e14bbd5ca25e07114a66e97d
NAYAN SONI <107099130+testgithubnayan@users.noreply.github.com>
2022-11-28 10:13:47 +0530

    Main method

--- StacksAndQueueMain.java
+++ StacksAndQueueMain.java
@@ -1,12 +1,12 @@
 package com.bridgelabz.stackandqueues.stacks;
 
-public class Main {
+public class StacksAndQueueMain {
 
     public static void main(String[] args) {
-        Queues<Integer> queue = new Queues<>();
-        queue.enqueu(56);
-        queue.enqueu(30);
-        queue.enqueu(70);
-        queue.print();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.print();
     }
 }
